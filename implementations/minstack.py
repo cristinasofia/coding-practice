a0e84fb1ed12a8bc68ed72596f917bdd42723270
Cristina Alonso <cristina.s.alon814@gmail.com>
2020-09-11 17:14:37 -0400

    commit

---
+++ implementations/minstack.py
@@ -0,0 +1,27 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+    
+    def push(self, x):
+        if not self.stack:
+            self.stack.append((x,x))
+            return
+        current_min = self.stack[-1][1]
+        self.stack.append((x, min(x, current_min)))
+    
+    def pop(self):
+        self.stack.pop()
+    
+    def top(self):
+        return self.stack[-1][0]
+    
+    def getMin(self):
+        return self.stack[-1][1]
+
+def main():
+    obj = MinStack()
+    obj.push(1)
+
+
+if __name__ == "__main__":
+    main()
--- software development/mathematics.md
+++ software development/mathematics.md
@@ -1,5 +1,11 @@
 ## Sum formulas
-1. Gauss
-   \sum_{x=1}^n$$ = 1 + 2 + 3 + ... + n = \frac{n(n + 1)}{2}
-
-2. 
+### Sum of consecutive integers
+   $\sum_{x=1}^n = 1 + 2 + 3 + ... + n = \frac{n(n + 1)}{2}$
+   $\sum_{x=1}^n = 1 + 2 + 3 + ... + n = \frac{(m + n)(n - m + 1)}{2}$
+### Arithmetic Progression
+  If initial term is $a_1$ and the common difference is $d$, then the $n-th$ term is:
+  $a_n = a_q + (n-1)d$
+  The sum 
+### Geometric Progression
+### Harmonic Sum
+### Logarithms
