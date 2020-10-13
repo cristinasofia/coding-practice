386b5dc28adcc6278fac9cfa5097ae4a15025bc2
Cristina Alonso <cristina.s.alon814@gmail.com>
2020-10-13 10:43:35 -0400

    solved

--- dynamic/python/longest-increasing-subsequence.py
+++ dynamic/python/longest-increasing-subsequence.py
@@ -1,1 +1,21 @@
-# ] https://leetcode.com/problems/longest-increasing-subsequence/
+# 300 https://leetcode.com/problems/longest-increasing-subsequence/
+
+def lengthOfLIS(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    if not nums:
+        return 0
+    
+    dp = [1]*len(nums)
+    max_ans = 1
+    
+    for i in range(1, len(nums)):
+        for j in range(i):
+            if nums[i] > nums[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+        
+        max_ans = max(max_ans, dp[i])
+        
+    return max_ans
